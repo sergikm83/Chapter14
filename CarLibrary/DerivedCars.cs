65ba0f803226c2b4d26d18c5dd215391b560545e
Sergey Misko <spy_3462@mail.ru>
2022-07-18 15:21:19 +0400

    добавил класс MiniVan

--- CarLibrary/DerivedCars.cs
+++ CarLibrary/DerivedCars.cs
@@ -16,4 +16,16 @@ namespace CarLibrary
             Console.WriteLine("Ramming speed! Faster is better...");
         }
     }
+    public class MiniVan : Car
+    {
+        public MiniVan() { }
+        public MiniVan(string name, int maxSp, int currSp)
+            : base(name, maxSp, currSp) { }
+        public override void TurboBoost()
+        {
+            // Минивэны имеют плохие возможности ускорения! :)
+            engState = EngineState.engineDead;
+            Console.WriteLine("Eek! Your engine block exploded!");
+        }
+    }
 }
