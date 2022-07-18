84129daeeed50b3a877e59ee6c7cb4bd94ceebfd
Sergey Misko <spy_3462@mail.ru>
2022-07-18 16:11:44 +0400

    добавил новый проект CSharpCarClient. Добавил ссылку на CarLibrary

---
+++ CSharpCarClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpCarClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
